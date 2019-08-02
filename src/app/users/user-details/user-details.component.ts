47a4a426ebbdabfc882340f17978a8b38ac2655d
nachshong <28922640+nachshong@users.noreply.github.com>
2019-08-02 17:40:46 +0300

    Move `user-details` to work with switchMap.

--- src/app/users/user-details/user-details.component.ts
+++ src/app/users/user-details/user-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 import { User } from '../user';
 import { UsersService } from '../users.service'
@@ -10,22 +12,26 @@ import { UsersService } from '../users.service'
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent implements OnInit {
-
   user: User;
   counter: number;
+  obUser: Observable<User>;
 
   constructor(private usersService: UsersService, private route: ActivatedRoute) { 
     this.user = new User();
+    
+    // Use switchMap to combine observables. switchMap is the preferred operation,
+    // because its behavior to emitting values only from the most recently projected Observable.
+    this.obUser = this.route.params.pipe(switchMap(params => {
+      var userId = Number.parseInt(params['id']);
+      return this.usersService.getUser(userId);
+    }));
   }
 
   ngOnInit() {
     this.counter = this.usersService.counter.next();
-
-    this.route.params.subscribe(params => { 
-        var userId = Number.parseInt(params['id']);
-        this.usersService.getUser(userId).subscribe(
-          value => this.user = value
-        );
-    });
+    
+    this.obUser.subscribe(
+      value => this.user = value
+    );
   }
 }
